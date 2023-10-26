7430c852797ea815537a39ad54a067f0c0f30ccc
TPIOS <116010062@link.cuhk.edu.cn>
2023-10-26 12:48:41 -0500

    create a random map generator

---
+++ i wanna record my jumps 1.03/I Wanna Record my Jumps 1.03/randomMapGenerator.py
@@ -0,0 +1,38 @@
+# total map is 800x608, one block is 32x32
+# any pairs of block should at least have 32 pixel shift 
+# to make map sparse
+import random
+def checkPos(x, y, bigMap, blockSize):
+    # print(x, y)
+    if x % blockSize != 0:
+        return False
+    if y % blockSize != 0:
+        return False
+    if bigMap[x][y] == 1:
+        return False
+    return True
+
+outputFile = "random_5.map"
+f = open(outputFile, "w")
+f.write(" 1.030000\n")
+f.write("random_1\n")
+f.write("Jushi_Gen\n")
+Nblock = 40
+blockSize = 32
+mapWidth = 800
+mapHeight = 608
+okMap = [[ 0 for i in range(mapHeight) ] for j in range(mapWidth) ]
+for i in range(Nblock):
+    x = random.randint(0, mapWidth-blockSize)
+    y = random.randint(0, mapHeight-blockSize)
+    while not checkPos(x, y, okMap, blockSize):
+        x = random.randint(0, mapWidth-blockSize)
+        y = random.randint(0, mapHeight-blockSize)
+    for j in range(x-2*blockSize, x+2*blockSize):
+        for k in range(y-2*blockSize, y+2*blockSize):
+            if j < 0 or j >= mapWidth or k < 0 or k >= mapHeight:
+                continue
+            okMap[j][k] = 1
+    f.write(" " + str(x) + " " + str(y) + " 2")
+f.write(" 128 512 3")
+f.close()
